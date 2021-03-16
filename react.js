09bb5dc50f07e59370ca6aa7c32b8dc09c927cde
Basel Alaktaa <basel.alaktaa.94@gmail.com>
2021-03-16 20:44:56 +0100

    1

--- react.js
+++ react.js
@@ -1,2 +1,16 @@
 const basel = -1;
-console.log(basel);
+console.log(basel
+
+const { program } = require('commander');
+program.version('0.0.1');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class MyComponent extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+ReactDOM.render(<MyComponent />, node);
